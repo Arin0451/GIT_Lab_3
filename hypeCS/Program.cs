d9558aa81982c15a556b57e6cc53cbb95dd59ed1
Arin <dprus18@gmail.com>
2026-05-27 18:06:19 +0300

    добавлено деление

--- hypeCS/Program.cs
+++ hypeCS/Program.cs
@@ -24,5 +24,10 @@ while (true)
             B = Convert.ToDouble(Console.ReadLine());
             break;
 
+        case 6:
+            Console.WriteLine("Выполнить операцию '/':");
+            result = A / B;
+            Console.WriteLine($"{A} / {B} =  {result}");
+            break;
     }
 }
