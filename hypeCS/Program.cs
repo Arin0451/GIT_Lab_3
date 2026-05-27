ff228f78ac0c374411007a1e8149aac5b74a22e8
Arin <dprus18@gmail.com>
2026-05-27 17:35:08 +0300

    1 комит

---
+++ hypeCS/Program.cs
@@ -0,0 +1,30 @@
+﻿double A = 0, B = 0, result;
+int choose;
+
+while (true)
+{
+    Console.WriteLine("Меню выбора \n выберете действие");
+    Console.WriteLine("1: Ввести А");
+    Console.WriteLine("2: Ввести В");
+    Console.WriteLine("3: Выполнить операцию '+' ");
+    Console.WriteLine("4: Выполнить операцию '-' ");
+    Console.WriteLine("5: Выполнить операцию '*' ");
+    Console.WriteLine("6: Выполнить операцию '/' ");
+    choose = Convert.ToInt32(Console.ReadLine());
+
+    switch (choose)
+    {
+        case 1:
+            Console.WriteLine("Введите значение для A:");
+            A = Convert.ToDouble(Console.ReadLine());
+            break;
+
+        case 2:
+            Console.WriteLine("Введите значение для B:");
+            B = Convert.ToDouble(Console.ReadLine());
+            break;
+
+
+
+    }
+}
